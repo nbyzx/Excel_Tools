65fc7fd9a76f7fb65af9e92a748672c8b7bc6ef1
nbyzx <15757408666@139.com>
2024-02-18 11:16:48 +0800

    Add .gitignore file

--- Ui/toolbar.py
+++ Ui/toolbar.py
@@ -1,7 +1,6 @@
 from PyQt6.QtCore import Qt, QStandardPaths
 from PyQt6.QtGui import QAction, QIcon, QPixmap, QPainter
-from PyQt6.QtWidgets import QToolBar, QFileDialog, QTableWidget, QTableWidgetItem, QMessageBox, QPushButton, QMenu, \
-    QToolButton
+from PyQt6.QtWidgets import QToolBar, QFileDialog, QTableWidget, QTableWidgetItem, QMessageBox, QMenu, QToolButton
 
 
 class ToolBar(QToolBar):
@@ -9,6 +8,7 @@ class ToolBar(QToolBar):
         super().__init__(parent)
         self.parent = parent
         self.setMinimumSize(24, 24)
+        self.open_file_button = QAction(QIcon('./img/toolbar/open_file.png'), '打开文件', self)
         self.as_save_button = QAction(QIcon('./img/toolbar/as_save.png'), "另存为", self)
         self.sort_button = QAction(QIcon('./img/toolbar/sort.png'), "排序", self)
         self.to_png_button = QToolButton()
@@ -20,9 +20,14 @@ class ToolBar(QToolBar):
         self.to_png_button_no_vheader = QAction("不带行表头", self)
         self.to_png_menu.addAction(self.to_png_button_no_header)
         self.to_png_menu.addAction(self.to_png_button_no_vheader)
+        self.left_justifying_button = QAction(QIcon('./img/toolbar/left_justifying.png'), "左对齐", self)
+        self.horizontally_button = QAction(QIcon('./img/toolbar/horizontally.png'), "水平居中", self)
+        self.right_justifying_button = QAction(QIcon('./img/toolbar/right_justifying.png'), "右对齐", self)
         self.init_ui()
 
     def init_ui(self):
+        self.open_file_button.triggered.connect(self.parent.menu_bar.open_file)
+        self.addAction(self.open_file_button)
         self.as_save_button.triggered.connect(self.parent.menu_bar.save_as_file)
         self.addAction(self.as_save_button)
         self.to_png_button.setMenu(self.to_png_menu)  # 设置下拉菜单
@@ -34,6 +39,21 @@ class ToolBar(QToolBar):
         self.sort_button.setCheckable(True)  # 设置为可选中
         self.sort_button.triggered.connect(self.sort)
         self.addAction(self.sort_button)
+        self.left_justifying_button.triggered.connect(
+            lambda: self.text_align(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter))
+        self.horizontally_button.triggered.connect(lambda: self.text_align(Qt.AlignmentFlag.AlignCenter))
+        self.right_justifying_button.triggered.connect(
+            lambda: self.text_align(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter))
+        self.addAction(self.left_justifying_button)
+        self.addAction(self.horizontally_button)
+        self.addAction(self.right_justifying_button)
+
+    def text_align(self, types: Qt.AlignmentFlag):
+        table = self.parent.select_table
+        # 获取选中的行和列
+        selected_items = table.selectedItems()
+        for selected_item in selected_items:
+            selected_item.setTextAlignment(types)
 
     def sort(self):
         table = self.parent.select_table
@@ -67,17 +87,17 @@ class ToolBar(QToolBar):
         for col in selected_cols:
             table_width += table.columnWidth(col)
             title = table.horizontalHeaderItem(col).text()
-            new_table_widget.setHorizontalHeaderItem(col-min(selected_cols), QTableWidgetItem(title))
-            new_table_widget.setColumnWidth(col-min(selected_cols), table.columnWidth(col))
+            new_table_widget.setHorizontalHeaderItem(col - min(selected_cols), QTableWidgetItem(title))
+            new_table_widget.setColumnWidth(col - min(selected_cols), table.columnWidth(col))
         for row in selected_rows:
             table_height += table.rowHeight(row)
-            new_table_widget.setRowHeight(row-min(selected_rows), table.rowHeight(row))
+            new_table_widget.setRowHeight(row - min(selected_rows), table.rowHeight(row))
         new_table_widget.setMinimumSize(table_width, table_height)
         new_table_widget.viewport().setMinimumSize(table_width, table_height)
         # 复制选中的行和列数据到新的QTableWidget
         for item in selected_items:
-            row = item.row()-min(selected_rows)
-            col = item.column()-min(selected_cols)
+            row = item.row() - min(selected_rows)
+            col = item.column() - min(selected_cols)
             new_item = QTableWidgetItem(item.text())
             new_table_widget.setItem(row, col, new_item)
 
